6f5d3e95894f9553e8d3996104f849271c4f3924
Jaden.Tang <jaden.tang@nexcel.vn>
2020-10-12 17:33:10 +0700

    Implemented Radial Buttons

---
+++ lib/common/radial_menu/radial_button.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:vector_math/vector_math.dart' show radians;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class RadialButton extends StatelessWidget {
+  final AnimationController controller;
+  final double angle, elevation;
+  final Color color;
+  final IconData icon;
+  final Function onPressed;
+
+  final Animation<double> translation;
+
+  RadialButton(this.controller, this.angle, this.onPressed,
+      {this.color = Colors.green,
+      this.icon = FontAwesomeIcons.accessibleIcon,
+      this.elevation = 0,
+      Key key})
+      : translation = Tween<double>(
+          begin: 0.0,
+          end: 60.0,
+        ).animate(
+          CurvedAnimation(parent: controller, curve: Curves.elasticOut),
+        ),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double rad = radians(angle);
+
+    return Transform(
+        transform: Matrix4.identity()
+          ..translate(
+              (translation.value) * cos(rad), (translation.value) * sin(rad)),
+        child: FloatingActionButton(
+            onPressed: onPressed,
+            child: Icon(icon),
+            backgroundColor: color,
+            elevation: elevation));
+  }
+}
--- lib/screens/lobby.dart
+++ lib/screens/lobby.dart
@@ -1,8 +1,7 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:hembo/common/radial_menu/radial_button.dart';
+import 'package:hembo/common/radial_menu/radial_menu.dart';
 import 'package:provider/provider.dart';
 import '../models/order.dart';
 
@@ -24,8 +23,6 @@ class _LobbyLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // var textTheme = Theme.of(context).textTheme.headline6;
-
     return Column(
       children: [
         Row(
@@ -47,16 +44,38 @@ class _Table extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    debugPrint("rebuilding _Table... $id");
+
+    var model = context.select<OrderTracker, TableModel>(
+        (tracker) => tracker.getTable(this.id));
+
     return Padding(
-      padding: EdgeInsets.all(10),
-      child: SizedBox(
-        height: 100,
-        width: 100,
-        child: Container(
-          color: context.select<OrderTracker, Color>(
-              (tracker) => tracker.getTable(this.id).getStatusColor()),
-        ),
-      ),
-    );
+        padding: const EdgeInsets.all(25),
+        child: RadialMenu(
+          onPressed: ({String event}) {
+            model.changeStatus();
+          },
+          buttonsBuilder: (radialAnimationController) => [
+            RadialButton(radialAnimationController, 0, () {
+              radialAnimationController.reverse();
+            }, color: Colors.red, icon: FontAwesomeIcons.thumbtack),
+            // RadialButton(controller, 45, _close,
+            //     color: Colors.green, icon: FontAwesomeIcons.sprayCan),
+            // RadialButton(controller, 90, _close,
+            //     color: Colors.orange, icon: FontAwesomeIcons.fire),
+            // RadialButton(controller, 135, _close,
+            //     color: Colors.blue, icon: FontAwesomeIcons.kiwiBird),
+          ],
+          // child: RaisedButton(
+          //   key: ValueKey(model.id),
+          //   color: context
+          //       .select<OrderTracker, Color>((tracker) => model.getStatusColor()),
+          //   child: Text(
+          //     model.id.toString(),
+          //     style: Theme.of(context).textTheme.headline3,
+          //   ),
+          //   onPressed: () => null,
+          // ),
+        ));
   }
 }
