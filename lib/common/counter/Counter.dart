78e8c5c367b928bfee8ce057ae0bf7bfac2c2a1c
dungtang93 <dungtang93@gmail.com>
2020-10-20 23:45:40 +0700

    Add text field to counter

--- lib/common/counter/Counter.dart
+++ lib/common/counter/Counter.dart
@@ -2,24 +2,46 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class Counter extends StatelessWidget {
+  final textEditingController = TextEditingController(text: '0');
+
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.centerRight,
-      child: ButtonBar(
-        alignment: MainAxisAlignment.spaceBetween,
-        children: [
-          FloatingActionButton(
-            heroTag: '1', //TODO as parameter
-            child: Icon(FontAwesomeIcons.arrowCircleLeft),
-            onPressed: () {},
-          ),
-          FloatingActionButton(
-            heroTag: '2',
-            child: Icon(FontAwesomeIcons.arrowCircleRight),
-            onPressed: () {},
-          ),
-        ],
+    return Container(
+      // width: 200,
+      height: 80,
+      color: Colors.black12,
+      child: Align(
+        alignment: Alignment.centerRight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(width: 6),
+            FloatingActionButton(
+              heroTag: '1', //TODO as parameter
+              child: Icon(FontAwesomeIcons.plusCircle),
+              onPressed: () {
+                //TODO: increment/decrement this value
+                textEditingController.text = (int.parse(textEditingController.text) + 1).toString();
+                debugPrint("Typed ${textEditingController.text}");
+              },
+            ),
+            SizedBox(width: 8),
+            Expanded(
+              child: TextField(
+                  controller: textEditingController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(border: const OutlineInputBorder()),
+                  textAlign: TextAlign.center),
+            ),
+            SizedBox(width: 8),
+            FloatingActionButton(
+              heroTag: '2',
+              child: Icon(FontAwesomeIcons.minusCircle),
+              onPressed: () {},
+            ),
+            SizedBox(width: 6),
+          ],
+        ),
       ),
     );
   }
