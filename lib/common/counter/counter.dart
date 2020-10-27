651948fba174cb4903b7971318fe27704d31b90a
Jaden.Tang <jaden.tang@nexcel.vn>
2020-10-27 16:21:15 +0700

    Menu wrap line items in Card widget

--- lib/common/counter/counter.dart
+++ lib/common/counter/counter.dart
@@ -20,79 +20,76 @@ class Counter extends StatelessWidget {
         super(key: key);
 
   @override
-  Widget build(BuildContext context) => Container(
-        // width: 200,
-        height: 100,
-        color: Colors.black12,
-        child: Align(
-          alignment: Alignment.centerRight,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const SizedBox(width: 6),
-              if (imagePath != null)
-                SizedBox(
-                  height: 95,
-                  child: Image.asset(
-                    imagePath,
-                    fit: BoxFit.contain,
-                  ),
-                ),
-              if (subtitle != null)
-                Expanded(
-                  child: Text(
-                    subtitle,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.headline6,
-                    softWrap: true,
-                    maxLines: 2,
-                  ),
-                ),
-              const SizedBox(width: 6),
-              FloatingActionButton(
-                // decrease
-                heroTag: null,
-                child: Icon(FontAwesomeIcons.minusCircle),
-                onPressed: () {
-                  var value = int.tryParse(textEditingController.text);
-                  if (value == null || value <= 0) return;
-
-                  value--;
-                  textEditingController.text = (value).toString();
-                  onDecrement?.call(value);
-                },
-              ),
-              const SizedBox(width: 6),
+  Widget build(BuildContext context) => Card(
+        borderOnForeground: false,
+        color: Colors.grey[150],
+        elevation: 4.0,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(width: 6),
+            if (imagePath != null)
               SizedBox(
-                width: 60,
-                // text box
-                child: TextField(
-                    controller: textEditingController,
-                    enabled: false,
-                    keyboardType:
-                        const TextInputType.numberWithOptions(signed: true),
-                    decoration:
-                        InputDecoration(border: const OutlineInputBorder()),
-                    textAlign: TextAlign.center),
+                height: 95,
+                child: Image.asset(
+                  imagePath,
+                  fit: BoxFit.contain,
+                ),
               ),
-              const SizedBox(width: 6),
-              FloatingActionButton(
-                // increase
-                heroTag: null,
-                child: Icon(FontAwesomeIcons.plusCircle),
-                onPressed: () {
-                  var value = int.tryParse(textEditingController.text);
-                  if (value == null || value < 0) return;
+            if (subtitle != null)
+              Expanded(
+                child: Text(
+                  subtitle,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.headline6,
+                  softWrap: true,
+                  maxLines: 2,
+                ),
+              ),
+            const SizedBox(width: 6),
+            FloatingActionButton(
+              // decrease
+              heroTag: null,
+              child: Icon(FontAwesomeIcons.minusCircle),
+              onPressed: () {
+                var value = int.tryParse(textEditingController.text);
+                if (value == null || value <= 0) return;
 
-                  value++;
-                  textEditingController.text = (value).toString();
-                  onIncrement?.call(value);
-                },
-              ),
-              const SizedBox(width: 6),
-            ],
-          ),
+                value--;
+                textEditingController.text = (value).toString();
+                onDecrement?.call(value);
+              },
+            ),
+            const SizedBox(width: 6),
+            SizedBox(
+              width: 60,
+              // text box
+              child: TextField(
+                  controller: textEditingController,
+                  enabled: false,
+                  keyboardType:
+                      const TextInputType.numberWithOptions(signed: true),
+                  decoration:
+                      InputDecoration(border: const OutlineInputBorder()),
+                  textAlign: TextAlign.center),
+            ),
+            const SizedBox(width: 6),
+            FloatingActionButton(
+              // increase
+              heroTag: null,
+              child: Icon(FontAwesomeIcons.plusCircle),
+              onPressed: () {
+                var value = int.tryParse(textEditingController.text);
+                if (value == null || value < 0) return;
+
+                value++;
+                textEditingController.text = (value).toString();
+                onIncrement?.call(value);
+              },
+            ),
+            const SizedBox(width: 6),
+          ],
         ),
       );
 }
