191796bc8787449fe5c6861e2111c2773945c779
Jaden.Tang <jaden.tang@nexcel.vn>
2020-11-02 17:16:14 +0700

    Add more assets, import Tuple

--- lib/models/dish.dart
+++ lib/models/dish.dart
@@ -1,19 +1,37 @@
 import 'dart:collection';
 
 import 'package:flutter/foundation.dart';
+import 'package:tuple/tuple.dart';
 
-const menu = {
-  0: 'Sample',
-  1: 'Fish Tuna',
-  2: 'Sushi Salmon',
-  3: 'An armpit',
-  4: 'Broken rice',
-  5: 'Beef Noddle',
-  6: 'Naruto-kun',
-  7: '@@@@@@@@@@',
-  8: 'Banh Mi',
-  9: 'Pho',
-  10: 'A very long text like lorem ipsum that should be three-dotted',
+const Map<int, Tuple2<String, String>> menu = {
+  0: Tuple2(
+    'Rice Noodles',
+    'assets/rice_noodles.jpg',
+  ),
+  1: Tuple2(
+    'Lime Juice',
+    'assets/lime_juice.jpg',
+  ),
+  2: Tuple2(
+    'Vegan Noodle',
+    'assets/vegan_noodles.jpg',
+  ),
+  3: Tuple2(
+    'Oatmeal with Berries and Coconut',
+    'assets/oatmeal_with_berries_and_coconut.jpg',
+  ),
+  4: Tuple2(
+    'Fried Chicken with Egg',
+    'assets/fried_chicken-with_with_wit_egg.jpg',
+  ),
+  5: Tuple2(
+    'Kimchi',
+    'assets/kimchi.jpg',
+  ),
+  6: Tuple2(
+    'Coffee',
+    'assets/coffee.jpg',
+  ),
 };
 
 @immutable
@@ -31,13 +49,13 @@ class Dish {
   operator ==(other) => other is Dish && other.id == id;
   int get hashCode => id;
 
-  const Dish(this.id, this.dish, [this.imagePath = 'assets/default.png']);
+  const Dish(this.id, this.dish, [this.imagePath]);
 
   /// Index of menu is the unique ID of associated [Dish]
   static UnmodifiableListView<Dish> getMenu() => UnmodifiableListView(
         List.generate(
           menu.length - 1,
-          (index) => Dish(index, menu[index]),
+          (index) => Dish(index, menu[index].item1, menu[index].item2),
           growable: false,
         ),
       );
