de6596637f9ec2ca92e966a3494e67c725b109a2
tcd93 <dungtang93@gmail.com>
2021-05-21 16:48:00 +0700

    UX: show all tooltips on long tap

--- lib/screens/history/second_tab/order_linechart.dart
+++ lib/screens/history/second_tab/order_linechart.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:fl_chart/fl_chart.dart';
@@ -43,59 +45,85 @@ class HistoryOrderLineChart extends StatelessWidget {
   }
 
   Widget _drawLineChart(BuildContext context, List<List<dynamic>> groupedData) {
+    var showTooltipsOnAllSpots = false;
     final _spots = _mapGroupDataToSpots(groupedData);
+    final _mainChart = LineChartBarData(
+      spots: _spots,
+      colors: [
+        RallyColors.primaryColor,
+      ],
+      barWidth: 2,
+      isStrokeCapRound: true,
+      dotData: FlDotData(show: true),
+      isCurved: true,
+      curveSmoothness: 0.2,
+      preventCurveOverShooting: true,
+      belowBarData: BarAreaData(show: false),
+    );
     return _spots.isEmpty
         ? Center(child: Text('No data'))
-        : LineChart(
-            LineChartData(
-              backgroundColor: RallyColors.primaryBackground,
-              lineTouchData: LineTouchData(
-                touchCallback: (LineTouchResponse touchResponse) {},
-                handleBuiltInTouches: true,
+        : StatefulBuilder(
+            builder: (context, setState) => LineChart(
+              LineChartData(
+                backgroundColor: RallyColors.primaryBackground,
+                lineTouchData: LineTouchData(
+                  // show tooltips on all spots on long tap
+                  touchCallback: (LineTouchResponse touchResponse) {
+                    Timer? _timer;
+                    if (touchResponse.touchInput.down) {
+                      _timer = Timer(Duration(seconds: 1), () {
+                        setState(() {
+                          showTooltipsOnAllSpots = touchResponse.touchInput.down;
+                        });
+                      });
+                    } else {
+                      _timer?.cancel();
+                      setState(() {
+                        showTooltipsOnAllSpots = touchResponse.touchInput.down;
+                      });
+                    }
+                  },
+                  // must disable this for showingTooltipIndicators to work
+                  handleBuiltInTouches: !showTooltipsOnAllSpots,
+                  touchSpotThreshold: 20.0,
+                ),
+                borderData: FlBorderData(
+                  show: true,
+                  border: Border(
+                    left: BorderSide(color: RallyColors.gray),
+                    bottom: BorderSide(color: RallyColors.gray),
+                  ),
+                ),
+                titlesData: FlTitlesData(
+                  leftTitles: SideTitles(
+                    showTitles: true,
+                    getTextStyles: Theme.of(context).textTheme.bodyText2 != null
+                        ? (value) => Theme.of(context).textTheme.bodyText2!
+                        : null,
+                    margin: 12.0,
+                    interval: _interval(groupedData),
+                  ),
+                  bottomTitles: SideTitles(
+                    showTitles: true,
+                    getTextStyles: Theme.of(context).textTheme.bodyText2 != null
+                        ? (value) => Theme.of(context).textTheme.bodyText2!
+                        : null,
+                    margin: 24.0,
+                    // convert index value back to yyyymmdd
+                    getTitles: (idx) => groupedData[idx.toInt()][0],
+                  ),
+                ),
+                gridData: FlGridData(show: false),
+                minY: 0.0,
+                showingTooltipIndicators: showTooltipsOnAllSpots
+                    ? [
+                        ..._spots.map(
+                          (spot) => ShowingTooltipIndicators([LineBarSpot(_mainChart, 0, spot)]),
+                        ),
+                      ]
+                    : [],
+                lineBarsData: [_mainChart],
               ),
-              borderData: FlBorderData(
-                show: true,
-                border: Border(
-                  left: BorderSide(color: RallyColors.gray),
-                  bottom: BorderSide(color: RallyColors.gray),
-                ),
-              ),
-              titlesData: FlTitlesData(
-                leftTitles: SideTitles(
-                  showTitles: true,
-                  getTextStyles: Theme.of(context).textTheme.bodyText2 != null
-                      ? (value) => Theme.of(context).textTheme.bodyText2!
-                      : null,
-                  margin: 12.0,
-                  interval: _interval(groupedData),
-                ),
-                bottomTitles: SideTitles(
-                  showTitles: true,
-                  getTextStyles: Theme.of(context).textTheme.bodyText2 != null
-                      ? (value) => Theme.of(context).textTheme.bodyText2!
-                      : null,
-                  margin: 24.0,
-                  // convert index value back to yyyymmdd
-                  getTitles: (idx) => groupedData[idx.toInt()][0],
-                ),
-              ),
-              gridData: FlGridData(show: false),
-              minY: 0.0,
-              lineBarsData: [
-                LineChartBarData(
-                  spots: _spots,
-                  colors: [
-                    RallyColors.primaryColor,
-                  ],
-                  barWidth: 2,
-                  isStrokeCapRound: true,
-                  dotData: FlDotData(show: true),
-                  isCurved: true,
-                  curveSmoothness: 0.2,
-                  preventCurveOverShooting: true,
-                  belowBarData: BarAreaData(show: false),
-                ),
-              ],
             ),
           );
   }
