b7759159efe2a7be76126288d30b00ddd9d2c1df
tcd93 <dungtang93@gmail.com>
2022-04-14 12:58:45 +0700

    Dynamic drawer items angles

--- lib/screens/lobby/table_icon.dart
+++ lib/screens/lobby/table_icon.dart
@@ -6,19 +6,130 @@ import '../../theme/rally.dart';
 import '../../provider/src.dart';
 import '../popup_del.dart';
 
-class TableIcon extends StatelessWidget {
+enum _Position {
+  topLeft,
+  left,
+  bottomLeft,
+  bottom,
+  bottomRight,
+  right,
+  topRight,
+  top,
+  center,
+
+  outside,
+  unknown,
+}
+
+class TableIcon extends StatefulWidget {
   final TableModel table;
 
-  const TableIcon({required this.table});
+  /// See [DraggableWidget.containerKey]
+  final GlobalKey? containerKey;
+  final Stream<Map<String, num>>? dragEndEventStream;
+
+  const TableIcon({
+    required this.table,
+    this.containerKey,
+    this.dragEndEventStream,
+  });
+
+  @override
+  State<TableIcon> createState() => _TableIconState();
+}
+
+class _TableIconState extends State<TableIcon> {
+  late Size size;
+  var position = _Position.unknown;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance!.addPostFrameCallback((_) {
+      size = (context.findRenderObject() as RenderBox).size;
+      determineOrientation({'id': widget.table.id, ...widget.table.getOffset()});
+    });
+    widget.dragEndEventStream?.listen(determineOrientation);
+  }
+
+  void determineOrientation(Map<String, num> event) {
+    if (event['id'] != widget.table.id) return;
+
+    final bgRenderObject = widget.containerKey?.currentContext?.findRenderObject();
+    if (bgRenderObject == null) return;
+
+    final bgRenderBox = bgRenderObject as RenderBox;
+    final centerX = event['x']! + size.height / 2; // the X pos at this element's center point
+    final centerY = event['y']! + size.width / 2; // the Y pos at this element's center point
+    const bandWidth = 140;
+
+    setState(() {
+      if (centerX - bandWidth <= 0 && centerX >= 0) {
+        if (centerY - bandWidth <= 0 && centerY >= 0) {
+          position = _Position.topLeft;
+        } else if (centerY - bandWidth >= 0 && centerY + bandWidth <= bgRenderBox.size.height) {
+          position = _Position.left;
+        } else {
+          position = _Position.bottomLeft;
+        }
+      } else if (centerX - bandWidth >= 0 && centerX + bandWidth <= bgRenderBox.size.width) {
+        if (centerY - bandWidth <= 0 && centerY >= 0) {
+          position = _Position.top;
+        } else if (centerY - bandWidth >= 0 && centerY + bandWidth <= bgRenderBox.size.height) {
+          position = _Position.center;
+        } else {
+          position = _Position.bottom;
+        }
+      } else if (centerX + bandWidth >= bgRenderBox.size.width &&
+          centerX <= bgRenderBox.size.width) {
+        if (centerY - bandWidth <= 0 && centerY >= 0) {
+          position = _Position.topRight;
+        } else if (centerY - bandWidth >= 0 && centerY + bandWidth <= bgRenderBox.size.height) {
+          position = _Position.right;
+        } else {
+          position = _Position.bottomRight;
+        }
+      } else {
+        position = _Position.outside;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    List<double> angles = [0, 90, 180];
+    switch (position) {
+      case _Position.topLeft:
+        angles = [0, 45, 90];
+        break;
+      case _Position.left:
+        angles = [-90, 0, 90];
+        break;
+      case _Position.bottomLeft:
+        angles = [0, -45, -90];
+        break;
+      case _Position.bottom:
+        angles = [0, -90, -180];
+        break;
+      case _Position.topRight:
+        angles = [-180, -225, -270];
+        break;
+      case _Position.right:
+        angles = [-90, -180, -270];
+        break;
+      case _Position.bottomRight:
+        angles = [-180, -135, -90];
+        break;
+      default:
+        angles = [0, 90, 180];
+    }
+
     return Padding(
       padding: const EdgeInsets.all(15),
       child: _RadialButton(
-        table,
+        widget.table,
         surroundingButtonsBuilder: (context, animController) =>
-            _build(context, animController, [0, 90, 180]),
+            _build(context, animController, angles),
       ),
     );
   }
@@ -39,11 +150,11 @@ class TableIcon extends StatelessWidget {
         key: const ValueKey<int>(1),
         child: FloatingActionButton(
           mini: true,
-          heroTag: 'menu-subtag-table-${table.id}',
+          heroTag: 'menu-subtag-table-${widget.table.id}',
           onPressed: () {
             Navigator.pushNamed(context, '/menu', arguments: {
-              'heroTag': 'menu-subtag-table-${table.id}',
-              'model': table,
+              'heroTag': 'menu-subtag-table-${widget.table.id}',
+              'model': widget.table,
             }).then((_) {
               Future.delayed(
                 const Duration(milliseconds: 600),
@@ -61,12 +172,12 @@ class TableIcon extends StatelessWidget {
         key: const ValueKey<int>(2),
         child: FloatingActionButton(
           mini: true,
-          heroTag: 'details-subtag-table-${table.id}',
-          onPressed: table.status == TableStatus.occupied
+          heroTag: 'details-subtag-table-${widget.table.id}',
+          onPressed: widget.table.status == TableStatus.occupied
               ? () {
                   Navigator.pushNamed(context, '/order-details', arguments: {
-                    'heroTag': 'details-subtag-table-${table.id}',
-                    'state': table,
+                    'heroTag': 'details-subtag-table-${widget.table.id}',
+                    'state': widget.table,
                     'from': 'lobby',
                   }).then((_) {
                     Future.delayed(
@@ -76,7 +187,7 @@ class TableIcon extends StatelessWidget {
                   });
                 }
               : null,
-          backgroundColor: table.status == TableStatus.occupied ? null : RallyColors.gray,
+          backgroundColor: widget.table.status == TableStatus.occupied ? null : RallyColors.gray,
           child: const Icon(Icons.receipt),
         ),
       ),
@@ -87,8 +198,8 @@ class TableIcon extends StatelessWidget {
         key: const ValueKey<int>(3),
         child: FloatingActionButton(
           mini: true,
-          heroTag: 'delete-subtag-table-${table.id}',
-          onPressed: () => _removeTable(context, table),
+          heroTag: 'delete-subtag-table-${widget.table.id}',
+          onPressed: () => _removeTable(context, widget.table),
           child: const Icon(Icons.delete),
         ),
       ),
